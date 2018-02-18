6b1d4755e36c6ebc20133e2c3b85502dcb99014c
aerya973 <r.belmonte@protonmail.com>
2018-02-18 20:51:45 +0100

    Nettoyage du dossier

--- webangency/js/main.js
+++
@@ -1,9 +0,0 @@
-function resizeIframe() {
-	$('iframe').attr("width", $(window).width());
-}
-$(function() {
-	//initial load
-	resizeIframe();
-});
-//on every window resize
-$(window).resize(resizeIframe);
